54c03def0a318d05ff5476dc7d54ebefcb1b30bc
LRC <lrc@klingonska.org>
2020-11-29 21:11:25 +0100

    Added test component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,54 @@
 <template>
-  [NAV]
+  <nav class="center">
+    <ul v-if="links.length">
+      <li
+        v-for="link in links"
+        :key="link.to"
+        class="border"
+      >
+        <router-link :to="link.to">{{ link.title }}</router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {
+      links: [
+        { title: "Test", to: "/test" }
+      ]
+    };
+  }
+});
+</script>
+
+<style scoped>
+nav {
+  margin-top: 2em;
+}
+
+nav ul {
+  margin: -1em;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+nav li {
+  list-style-type: none;
+  margin: 1em;
+  flex: 0 0 20%;
+}
+
+nav a {
+  padding: 1em;
+  color: inherit;
+  font-size: 2em;
+  text-decoration: none;
+  display: block;
+}
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,41 @@
+<template>
+  <h2>Test</h2>
+  <p>
+    <button class="button" @click="load">Run</button>
+  </p>
+  <PuzzleOutput :lines="lines" @clear="clearOutput" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useInput } from "@/functions/input";
+import { useOutput } from "@/functions/output";
+import PuzzleOutput from "@/components/PuzzleOutput.vue";
+
+export default defineComponent({
+  components: {
+    PuzzleOutput
+  },
+
+  setup() {
+    const input = useInput();
+    const output = useOutput();
+
+    return {
+      async load() {
+        const lines = await input.load("/inputs/test.txt");
+        lines[0].split(",").forEach(part => {
+          output.print(part, true);
+        });
+        output.print("");
+        output.error(lines[1]);
+        output.system(lines[2]);
+      },
+      lines: output.lines,
+      clearOutput() {
+        output.clear();
+      }
+    };
+  }
+});
+</script>
