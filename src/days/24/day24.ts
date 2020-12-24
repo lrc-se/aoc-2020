189194326798028ca6461cc1ab049a995970bef3
LRC <lrc@klingonska.org>
2020-12-24 13:07:37 +0100

    Day 24: updated component with busy state

--- src/days/24/day24.ts
+++ src/days/24/day24.ts
@@ -54,11 +54,9 @@ export function createHandler(output: OutputPublic) {
       runPuzzle1(input, output);
     },
     runTest2(input: string[]) {
-      output.system("Running test 2...");
       runPuzzle2(input, 100, output);
     },
     runPuzzle2(input: string[]) {
-      output.system("Running puzzle 2...");
       runPuzzle2(input, 100, output);
     }
   };
