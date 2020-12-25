deade5e0eb710915b90630063a882613255e5672
LRC <lrc@klingonska.org>
2020-12-25 10:17:00 +0100

    Day 25: solved puzzle

---
+++ src/days/25/day25.ts
@@ -0,0 +1,65 @@
+import { OutputPublic } from "@/functions/output";
+
+interface Keys {
+  card: number;
+  door: number;
+}
+
+interface KeyCache {
+  [K: number]: number;
+}
+
+function getKeys(input: string[]): Keys {
+  const keys = input.map(num => +num);
+  return {
+    card: keys[0],
+    door: keys[1]
+  };
+}
+
+function transform(subject: number, loops: number, cache: KeyCache = {}): number {
+  let value = 1;
+  let start = 1;
+  let existing = loops;
+  while (existing) {
+    if (cache[existing]) {
+      value = cache[existing];
+      start = existing + 1;
+      break;
+    }
+    --existing;
+  }
+
+  for (let i = start; i <= loops; ++i) {
+    value = (value * subject) % 20201227;
+  }
+  cache[loops] = value;
+  return value;
+}
+
+function runPuzzle(input: string[], output: OutputPublic) {
+  const keys = getKeys(input);
+  let loops = 0;
+  let key;
+  const cache: KeyCache = {};
+  do {
+    key = transform(7, ++loops, cache);
+  } while (key != keys.card);
+  output.print(`Card loop size: ${loops}`);
+  const encKey = transform(keys.door, loops);
+  output.print(`Encryption key: ${encKey}`);
+  output.print();
+}
+
+export function createHandler(output: OutputPublic) {
+  return {
+    runTest1(input: string[]) {
+      output.system("Running test 1...");
+      runPuzzle(input, output);
+    },
+    runPuzzle1(input: string[]) {
+      output.system("Running puzzle 1...");
+      runPuzzle(input, output);
+    }
+  };
+}
--- src/days/days.ts
+++ src/days/days.ts
@@ -190,6 +190,13 @@ export const days: Day[] = [
       { number: 1, hasTest: true, hasSolution: true },
       { number: 2, hasTest: true, testInput: 1, hasSolution: true }
     ]
+  },
+  {
+    number: 25,
+    title: "Combo Breaker",
+    puzzles: [
+      { number: 1, hasTest: true, hasSolution: true }
+    ]
   }
 ];
 
